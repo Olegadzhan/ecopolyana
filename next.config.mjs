b90f6016fb7badeba5e05eba47d2addbd4bdf38f
Olegadzhan <156513783+Olegadzhan@users.noreply.github.com>
2026-02-27 23:54:28 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,10 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'image.pollinations.ai',
       },
+      {
+        protocol: 'https',
+        hostname: '*.pollinations.ai',
+      },
     ],
   },
 };
